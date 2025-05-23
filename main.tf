a94bdde38426cb4eacd9c7dc0c338a40942a23f1
dpkcbe <dpkcbe@gmail.com>
2025-05-23 18:15:58 +0530

    IGW-NAT-PVT-PUB

--- main.tf
+++ main.tf
@@ -78,7 +78,8 @@ resource "aws_internet_gateway" "gw" {
 
 resource "aws_nat_gateway" "nat" {
   allocation_id = aws_eip.nat.id
-  subnet_id     = aws_subnet.public[0].id
+  subnet_id     = aws_subnet.public_1.id
+
   tags = {
     Name = "prefect-ecs"
   }
@@ -99,10 +100,36 @@ resource "aws_route_table" "public" {
   }
 }
 
-resource "aws_route_table_association" "public_assoc" {
-  count          = length(aws_subnet.public)
-  subnet_id      = aws_subnet.public[count.index].id
+# Public subnet associations
+resource "aws_route_table_association" "public_assoc_1" {
+  subnet_id      = aws_subnet.public_1.id
   route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public_assoc_2" {
+  subnet_id      = aws_subnet.public_2.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public_assoc_3" {
+  subnet_id      = aws_subnet.public_3.id
+  route_table_id = aws_route_table.public.id
+}
+
+# Private subnet associations
+resource "aws_route_table_association" "private_assoc_1" {
+  subnet_id      = aws_subnet.private_1.id
+  route_table_id = aws_route_table.private.id
+}
+
+resource "aws_route_table_association" "private_assoc_2" {
+  subnet_id      = aws_subnet.private_2.id
+  route_table_id = aws_route_table.private.id
+}
+
+resource "aws_route_table_association" "private_assoc_3" {
+  subnet_id      = aws_subnet.private_3.id
+  route_table_id = aws_route_table.private.id
 }
 
 resource "aws_route_table" "private" {
@@ -114,10 +141,4 @@ resource "aws_route_table" "private" {
   tags = {
     Name = "prefect-ecs-private"
   }
-}
-
-resource "aws_route_table_association" "private_assoc" {
-  count          = length(aws_subnet.private)
-  subnet_id      = aws_subnet.private[count.index].id
-  route_table_id = aws_route_table.private.id
 }
